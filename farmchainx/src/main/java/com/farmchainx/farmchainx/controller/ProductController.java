a8be900e30e6be61d40b5c7c95702356e3e1aa9c
Ashwanth <ashwanth.jayakumar.gowri@gmail.com>
2025-10-24 09:57:52 +0530

    QR Code Part2 Commit

--- farmchainx/src/main/java/com/farmchainx/farmchainx/controller/ProductController.java
+++ farmchainx/src/main/java/com/farmchainx/farmchainx/controller/ProductController.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +14,7 @@ import com.farmchainx.farmchainx.model.Product;
 import com.farmchainx.farmchainx.model.User;
 import com.farmchainx.farmchainx.repository.UserRepository;
 import com.farmchainx.farmchainx.service.ProductService;
-import com.farmchainx.farmchainx.service.QrService;
+
 
 @RestController
 @RequestMapping("/api/products")
@@ -21,12 +22,12 @@ public class ProductController {
 
     private final ProductService productService;
     private final UserRepository userRepository;
-    private final QrService qrService;
+   
 
-    public ProductController(ProductService productService, UserRepository userRepository, QrService qrService) {
+    public ProductController(ProductService productService, UserRepository userRepository) {
         this.productService = productService;
         this.userRepository = userRepository;
-        this.qrService = qrService;
+        
     }
 
   
@@ -97,6 +98,16 @@ public class ProductController {
     
     @PostMapping("/{id}/qrcode")
     public String generateProductQrCode(@PathVariable Long id) {
-    	return qrService.generateProductQr(id);
+    	return productService.generateProductQr(id);
+    }
+    
+    @GetMapping("/{id}/qrcode/download")
+    public ResponseEntity<byte[]> downloadProductQR(@PathVariable Long id){
+    	byte[] imageBytes = productService.getProductQRImage(id);
+    	
+    	return ResponseEntity.ok()
+    			.header("Content-Type", "image/png")
+    			.header("Content-Disposition", "attachment; filename=product_"+id+".png")
+    			.body(imageBytes);
     }
 }
